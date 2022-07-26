e3fd06db3c641d06b12413b262b6193f56e14230
leilaaua <leila0777@icloud.com>
2022-07-26 15:09:07 +0300

    Initial Commit

---
+++ PageView/PageViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  PageViewApp.swift
+//  PageView
+//
+//  Created by leila on 26.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct PageViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
